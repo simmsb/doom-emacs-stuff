7f9c345e65060fffe3c441e5de8654eddc2d9ecf
Ben Simms <ben@bensimms.moe>
2018-09-09 01:18:32 +0100

    push changes

---
+++ modules/feature/org-ref/config.el
@@ -0,0 +1,5 @@
+(def-package! org-ref
+  :config
+  (setq org-ref-bibliography-notes "~/org/bibliography/notes.org"
+        org-ref-default-bibliography '("~/dev/org/bibliography/references.bib")
+        org-ref-pdf-directory "~/org/bibliography/bibtext-pdfs/"))
