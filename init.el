413b2f96797855ba55ec4e191528bde8a33e088f
Ben Simms <ben@bensimms.moe>
2019-08-30 13:24:17 +0100

    push changes

--- init.el
+++ init.el
@@ -188,7 +188,7 @@
 (pcase (system-name)
   ("laptop"
    (toggle-frame-maximized)
-   (setq doom-theme 'doom-tomorrow-night-eighties)
+   (setq doom-theme 'doom-tomorrow-night)
    (setq doom-font (font-spec :family "Fira Mono" :size 14)
          doom-big-font (font-spec :family "Fira Mono" :size 18)))
   (_
